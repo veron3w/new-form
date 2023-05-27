367bf422aef6959332bb235b3a05f3b2ebd5be2e
veron3w <du_verone_19@hotmail.com>
2023-05-27 01:42:40 -0300

    new random JS

---
+++ index.js
@@ -0,0 +1,12 @@
+/* let nome = prompt("Olá, me diga seu nome por favor.")
+
+let horario = prompt("Que horas são?")
+
+if (horario > 0 && horario <= 12) {
+  alert("Bom dia, " + nome + ". Agora são exatamente " + horario + "hr(s) da manhã.")
+} else if(horario <= 18) {
+  alert("Boa tarde, " + nome + ". Agora são exatamente " + horario + "hr(s) da tarde.")
+} else if (horario <=24) {
+  alert("Boa noite, " + nome + ". Agora são exatamente " + horario + "hr(s) da noite.")
+}
+  */
